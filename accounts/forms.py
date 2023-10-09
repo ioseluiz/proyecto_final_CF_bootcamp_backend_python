ff804bf57c4319f690b4db7300599d3e6f37a367
ioseluiz <joseluism1412@gmail.com>
2023-10-08 22:41:20 -0500

    changes in navbar and login

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,11 +5,11 @@ from django.forms import EmailInput, TextInput, PasswordInput, CharField
 class CustomUserCreationForm(UserCreationForm):
     password1 = CharField(
         label="Password",
-        widget=PasswordInput(attrs={'class':'form-control', 'type':'password', 'placeholder':'Password'}),
+        widget=PasswordInput(attrs={'class':'form-control', 'type':'password', 'placeholder':'Password','required':'required'}),
     )
     password2 = CharField(
         label="Confirm password",
-        widget=PasswordInput(attrs={'class':'form-control', 'type':'password','placeholder':'Confirmar Password'}),
+        widget=PasswordInput(attrs={'class':'form-control', 'type':'password','placeholder':'Confirmar Password','required':'required'}),
     )
     class Meta:
         model = get_user_model()
@@ -32,9 +32,20 @@ class CustomUserCreationForm(UserCreationForm):
         }
 
 class CustomUserChangeForm(UserChangeForm):
+    password = CharField(
+        label="Password",
+        widget=PasswordInput(attrs={'class':'form-control', 'type':'password','placeholder':'Introduzca su password','required':'required'}),
+    )
+
     class Meta:
         model = get_user_model()
-        fields = (
-            "email",
+        fields = [
             "username"
-        )
+        ]
+        widgets = {
+            'username': TextInput(attrs={
+                'class': "form-control",
+                'placeholder': "Password",
+                'required': "required",
+            })
+        }
